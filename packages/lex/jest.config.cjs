6add0d304480d334f848aa34d72716b3e9392e90
Giraldo Rosales <giraldo@nitrogenlabs.com>
2025-02-04 13:43:55 -0600

    Fix package json

--- packages/lex/jest.config.cjs
+++ packages/lex/jest.config.cjs
@@ -2,54 +2,38 @@
  * Copyright (c) 2018-Present, Nitrogen Labs, Inc.
  * Copyrights licensed under the MIT License. See the accompanying LICENSE file for terms.
  */
-const base = require('../../jest.config.base');
-const pack = require('./package.json');
+const { createDefaultPreset } = require("ts-jest");
+const base = require("../../jest.config.base");
+const pack = require("./package.json");
 
 module.exports = {
   ...base,
   collectCoverage: true,
   coveragePathIgnorePatterns: [
-    '/node_modules/',
-    '(tests/.*.mock).(jsx?|tsx?)$'
+    "/node_modules/",
+    "(tests/.*.mock).(jsx?|tsx?)$",
   ],
-  coverageReporters: ['html', 'text'],
+  coverageReporters: ["html", "text"],
   coverageThreshold: {
     global: {
       branches: 80,
       functions: 90,
       lines: 80,
-      statements: 80
-    }
+      statements: 80,
+    },
   },
   displayName: pack.name,
-  extensionsToTreatAsEsm: ['.ts'],
-  moduleFileExtensions: ['js', 'ts', 'tsx', 'json'],
+  extensionsToTreatAsEsm: [".ts"],
+  moduleFileExtensions: ["js", "ts", "tsx", "json"],
   moduleNameMapper: {
-    '^(\\.{1,2}/.*)\\.js$': '$1'
+    "^(\\.{1,2}/.*)\\.js$": "$1",
   },
-  rootDir: './',
-  setupFiles: ['<rootDir>/jest.setup.ts'],
-  testMatch: ['<rootDir>/**/*.test.ts*'],
+  rootDir: "./",
+  setupFiles: ["<rootDir>/jest.setup.ts"],
+  testMatch: ["<rootDir>/**/*.test.ts*"],
   transform: {
-    '\\.[jt]sx?$': [
-      'ts-jest',
-      {
-        astTransformers: {
-          before: [
-            {
-              path: '../../node_modules/ts-jest-mock-import-meta',
-              options: {metaObjectReplacement: {url: 'https://www.url.com'}}
-            }
-          ]
-        },
-        diagnostics: {
-          ignoreCodes: [1343]
-        },
-        useESM: true
-      }
-    ]
+    ...createDefaultPreset().transform,
+    "\\.(gql|graphql)$": "jest-transform-graphql",
   },
-  transformIgnorePatterns: [
-    '!/node_modules/(?!execa)'
-  ]
+  transformIgnorePatterns: ["!/node_modules/(?!execa)"],
 };
--- packages/lex/jest.config.lex.js
+++ packages/lex/jest.config.lex.js
@@ -1,5 +1,5 @@
+import { createDefaultPreset } from "ts-jest";
 import { resolve as pathResolve } from "path";
-
 import { URL } from "url";
 
 import { getNodePath } from "./dist/utils/file.js";
@@ -9,7 +9,6 @@ const { jest, sourceFullPath, targetEnvironment, useTypescript } = JSON.parse(
   process.env.LEX_CONFIG || "{}"
 );
 
-// Polyfill path
 const dirName = new URL(".", import.meta.url).pathname;
 const nodePath = pathResolve(dirName, "./node_modules");
 
@@ -50,7 +49,6 @@ export default {
     "\\.(css|jpg|png|svg|txt)$": pathResolve(dirName, "./emptyModule"),
   },
   modulePaths: [rootDir, `${rootDir}/node_modules`, nodePath, sourceFullPath],
-  // preset: 'ts-jest/presets/js-with-ts-esm',
   resolver: pathResolve(dirName, "./resolver.cjs"),
   rootDir,
   setupFiles: [
@@ -63,24 +61,7 @@ export default {
   testRegex,
   testRunner: getNodePath("jest-circus/runner.js"),
   transform: {
-    "\\.[jt]sx?$": [
-      "ts-jest",
-      {
-        diagnostics: {
-          ignoreCodes: [1343],
-        },
-        astTransformers: {
-          before: [
-            {
-              path: getNodePath("ts-jest-mock-import-meta"),
-              options: {
-                metaObjectReplacement: { url: "https://www.url.com" },
-              },
-            },
-          ],
-        },
-      },
-    ],
+    ...createDefaultPreset().transform,
     "\\.(gql|graphql)$": "jest-transform-graphql",
   },
   transformIgnorePatterns,
